333c9a91e5fa994073d4e3f505b733fbe77ac7ef
Isaac Villicana <villicana.i@gmail.com>
2018-04-06 15:50:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,11 @@ def line(array)
     if array.empty?
       puts "The line is currently empty."
     else
-
-      puts "The line is currently: "
-      array.each.with_index do |name, index|
-        puts "#{index+1}. #{name} "
+      new_line = ""
+      array.map.with_index do |name, index|
+        new_line << "#{index+1}. #{name} "
       end
+      puts "The line is currently: #{new_line}""
+
     end
 end
