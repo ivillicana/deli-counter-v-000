74b95f3c1454859b861e28fd1acb2a0e0c44b6b9
Isaac Villicana <villicana.i@gmail.com>
2018-04-06 15:29:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,8 @@ def line(array)
       puts "The line is currently empty."
     else
 
+      puts "The list is currently: "
+      array.map.with_index do |name, index|
+        puts "#{index+1}. #{name}"
     end
 end
